94f40f1dae9aa68322ceea5d3c82955c3adfa7af
Merika Senior <seniormerika7@gmail.com>
2022-11-06 18:15:50 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+window.onload= function()
+{
+    searchhero();
+}
+
+function searchhero () 
+{
+    var httpRequest = new XMLHttpRequest();
+
+    var url = "superheroes.php";
+
+    document.getElementById("btn").onclick =  function() 
+    {
+               
+        httpRequest.onreadystatechange = getHeroes;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+        return false;
+    }
+
+        function getHeroes () 
+        {
+            var result = document.getElementById("result");
+
+            if (httpRequest.readyState === XMLHttpRequest.DONE)
+             {
+                if (httpRequest.status === 200)
+                {
+                    var res = httpRequest.responseText;
+                    result.innerHTML = res;
+                }
+                else {
+                    alert ('There was an error')
+                }
+
+            }
+            
+           
+        }
+   
+        
+
+    }
